7d6fe5f81d50d05a20d2240caf610287fd1f81c0
DroidStudioAS <as.droiddevteam@gmail.com>
2024-03-30 18:47:33 +0100

    added product card css and html to blade

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ProductModel;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public static function index()
+    {
+        $featuredProducts = ProductModel::latest()->take(6)->get();
+
+        //return value
+        return view("welcome", compact("featuredProducts"));
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,19 @@
 @section("title") HomePage @endsection
 
 @section("content")
-    Welcome
+    <h1>Featured Models</h1>
+
+       <div class="product_card">
+            <div class="product_content">
+                <div class="product_image_container">
+
+                </div>
+                <div class="product_data_short">
+
+                </div>
+            </div>
+           <div class="action_buttons">
+
+           </div>
+       </div>
 @endsection
