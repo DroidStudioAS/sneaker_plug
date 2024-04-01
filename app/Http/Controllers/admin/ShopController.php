429f4f3cc3766cb1593ae782f4178f2ca51c2797
DroidStudioAS <as.droiddevteam@gmail.com>
2024-04-01 12:07:40 +0200

    added admin product cards

--- app/Http/Controllers/admin/ShopController.php
+++ app/Http/Controllers/admin/ShopController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\ProductModel;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
     public function index(){
+        $products = ProductModel::all();
 
-        return view("admin.admin_products");
+        return view("admin.admin_products", compact("products"));
     }
 }
--- resources/views/admin/admin_products.blade.php
+++ resources/views/admin/admin_products.blade.php
@@ -1,4 +1,28 @@
 @extends("layouts.admin_layout")
 @section("admin_content")
-    heya
+    <div class="table_container">
+        @foreach($products as $product)
+            <div class="admin_product_card">
+                <div class="admin_product_row_prim">
+                    <div class="admin_image_container">
+                        <img src="{{asset("/res/mock.jpg")}}" alt="">
+                    </div>
+                    <div class="product_description">
+                        <p>{{$product->description}}</p>
+                    </div>
+                </div>
+                <div class="admin_product_row_sec">
+                    <div class="product_info_field">Brand: {{$product->category->name}}</div>
+                    <div class="product_info_field">Model: {{$product->Name}}</div>
+                    <div class="product_info_field">Available: {{$product->available_amount}}</div>
+                    <div class="product_info_field">Price: {{$product->price}} $</div>
+                    <div class="button_container">
+                        <div class="edit_button">Edit</div>
+                        <div class="delete_button">Delete</div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
 @endsection
