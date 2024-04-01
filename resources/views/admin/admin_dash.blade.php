b1b0a196412793f46b781f6a2dd5a3ad2dcf452a
DroidStudioAS <as.droiddevteam@gmail.com>
2024-04-01 09:00:42 +0200

    styled messge cards for admin

--- resources/views/admin/admin_dash.blade.php
+++ resources/views/admin/admin_dash.blade.php
@@ -2,12 +2,12 @@
 @section("admin_content")
 @foreach($messages as $message)
    <div class="message_card">
-       <p class="message_field">{{$message->subject}}</p>
-       <p class="message_field">{{$message->email}}</p>
-       <p class="message_field">{{$message->message}}</p>
+       <p class="message_field">Subject: {{$message->subject}}</p>
+       <p class="message_field">From: {{$message->email}}</p>
+       <p class="message_field">"{{$message->message}}"</p>
        <div class="button_container">
-           <button></button>
-           <button></button>
+           <button class="edit_button">Edit</button>
+           <button class="delete_button">Delete</button>
        </div>
    </div>
 @endforeach
