ce264be327fa0774cf2a8522e8816f57f3f25cf4
DroidStudioAS <as.droiddevteam@gmail.com>
2024-04-02 15:52:12 +0200

    added controller groups to web.php

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 /***********User Routes**********/
+//contact
+Route::controller(ContactController::class)->group(function(){
+    Route::get("/contact", "index")->name("contact");
+    Route::post("/contact/send", "sendMessage")->name("send");
+});
 //home
 Route::get("/", [HomeController::class, "index"])->name("home");
-//contact
-Route::get("/contact", [ContactController::class, "index"])->name("contact");
-Route::post("/contact/send", [ContactController::class, "sendMessage"])->name("send");
 //shop
 Route::get("/shop", [ShopController::class, "index"])->name("shop");
 //about
@@ -35,15 +37,22 @@ Route::get("/about", function (){
 Route::middleware(["auth", AdminMiddleware::class])
     ->prefix("/admin")
     ->group(function (){
-    Route::get("/",[\App\Http\Controllers\admin\ContactController::class, "index"])->name("admin_panel");
-    Route::get("/shop",[\App\Http\Controllers\admin\ShopController::class, "index"])->name("admin_shop");
-    Route::post("/edit-message/{contact}",[\App\Http\Controllers\admin\ContactController::class,"editMessage"]);
-    Route::post("/delete-message/{contact}",[ \App\Http\Controllers\admin\ContactController::class, "deleteMessage"]);
-    Route::post("/add-product",[\App\Http\Controllers\admin\ShopController::class,"addProduct"])->name("add_product");
-    Route::post("/edit-product/{product}",[\App\Http\Controllers\admin\ShopController::class,"editProduct"])->name("edit_product");
-    Route::post("/delete-product/{product}",[\App\Http\Controllers\admin\ShopController::class,"deleteProduct"])->name("delete_product");
-});
+        //contanct
+        Route::controller(\App\Http\Controllers\admin\ContactController::class)->group(function (){
+            Route::get("/","index")->name("admin_panel");
+            Route::post("/edit-message/{contact}","editMessage");
+            Route::post("/delete-message/{contact}", "deleteMessage");
+        });
 
+        //shop
+        Route::controller(\App\Http\Controllers\admin\ShopController::class)->group(function(){
+            Route::get("/shop", "index")->name("admin_shop");
+            Route::post("/add-product","addProduct")->name("add_product");
+            Route::post("/edit-product/{product}","editProduct")->name("edit_product");
+            Route::post("/delete-product/{product}","deleteProduct")->name("delete_product");
+        });
+
+});
 /***********Admin Routes End**********/
 
 
