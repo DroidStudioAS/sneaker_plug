e342729df7f597455174768cd539f70c9ee00882
DroidStudioAS <as.droiddevteam@gmail.com>
2024-04-03 14:05:13 +0200

    added cart and permalink logic to shop page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\ProductModel;
 use App\Repositories\ProductRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class HomeController extends Controller
 {
@@ -15,6 +16,7 @@ class HomeController extends Controller
     }
     public function index()
     {
+        //Session::forget("products");
         $featuredProducts = $this->productRepo->getLatestProducts(6);
         //return value
         return view("welcome", compact("featuredProducts"));
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -19,14 +19,52 @@
                 </div>
             </div>
             <div class="action_buttons">
-                <img id="add_to_cart_button" src="{{asset("/res/icon_cart.svg")}}" alt="add to cart" class="action_button">
-                <img id="more_info_button" src="{{asset("/res/icon_info.svg")}}" alt="more info" class="action_button"
-                onclick="window.location='{{route('product.permalink',['product'=>$product->id])}}'">
+                <img onclick="displayAmountToAddModule({{json_encode($product)}})"
+                     id="add_to_cart_button" src="{{asset("/res/icon_cart.svg")}}" alt="add to cart" class="action_button">
+                <img onclick="window.location='{{route('product.permalink',['product'=>$product->id])}}'"
+                     id="more_info_button" src="{{asset("/res/icon_info.svg")}}" alt="more info" class="action_button">
             </div>
         </div>
     @endforeach
     </div>
+    <form class="amount_module">
+        <img onclick="hideAmountToAddModule()" class="close_button" src="{{asset("/res/close.png")}}"/>
+        <p>Select Amount To Order</p>
+        <p id="available_display"></p>
+        <input class="input_text" type="number" name="amount" id="amount_input" min="1">
+        <input type="submit" value="Add To Cart" class="add_to_cart">
+    </form>
     <script>
+        function hideAmountToAddModule(){
+            $(".amount_module").css("display","none")
+        }
+        function displayAmountToAddModule(product){
+            $(".amount_module").css("display","flex")
+            $("#available_display").text("Available: " + product.available_amount);
+            $("#amount_input").attr("max", product.available_amount)
 
+            $(".add_to_cart").off("click").on("click",function (e){
+                e.preventDefault();
+                let amount = $("#amount_input").val()
+                console.log(amount);
+                if(amount>0 && amount<=product.available_amount && amount!==null) {
+                    addToCart(product, amount);
+                }
+            })
+        }
+        function addToCart(product, amount){
+            console.log("trigger");
+            $.ajax({
+                url:"/cart/add/"+product.id,
+                type:"POST",
+                data:{
+                    "_token": $('meta[name="csrf-token"]').attr('content'),
+                    "amount": amount
+                },
+                success:function(response){
+                    console.log(response);
+                }
+            })
+        }
     </script>
 @endsection
