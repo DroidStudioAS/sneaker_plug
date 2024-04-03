92cfa73374e61d84c5f3680ff8ba2105a76aebcf
DroidStudioAS <as.droiddevteam@gmail.com>
2024-04-03 07:38:31 +0200

    added action buttons to product card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,8 @@
                 </div>
             </div>
             <div class="action_buttons">
-
+                <img id="add_to_cart_button" src="{{asset("/res/icon_cart.svg")}}" alt="add to cart" class="action_button">
+                <img id="more_info_button" src="{{asset("/res/icon_info.svg")}}" alt="more info" class="action_button">
             </div>
         </div>
     @endforeach
