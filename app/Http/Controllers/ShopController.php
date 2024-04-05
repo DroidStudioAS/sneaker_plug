c732a8aa5ebae2bc08fab5c8248b99df73755e93
DroidStudioAS <as.droiddevteam@gmail.com>
2024-04-05 06:49:45 +0200

    implemented backedn search function

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -28,11 +28,41 @@ class ShopController extends Controller
         return view("product", compact("singleProduct", "totalAvailable"));
     }
     public function search(Request $request){
+        $sizeSent = false;
+        $amountSent = false;
         $products= ProductModel::where("Name", "LIKE", "%$request->Name%")
             ->where(["category_id"=>$request->category_id])
             ->where("price", "<=" ,$request->price)
             ->get();
-        dd($products);
+
+        if($request->size!==null){
+            $sizeSent=true;
+            if($request->amount!==null){
+                $amountSent=true;
+            }
+        }
+        $filteredProducts = collect([]);
+
+            foreach ($products as $product){
+                foreach ($product->availableSizes as $size){
+                    if($sizeSent && $amountSent){
+                        if($size->size==$request->size && $size->available>=$request->amount){
+                            $filteredProducts->push($product);
+                        }
+                    }
+                    else if(!$sizeSent && $amountSent){
+                        if($size->available>=$request->amount){
+                            $filteredProducts->push($product);
+                        }
+                    }
+                    else if($sizeSent && !$amountSent){
+                        if($size->size==$request->size){
+                            $filteredProducts->push($product);
+                        }
+                    }
+                }
+            }
+        dd($filteredProducts);
 
     }
 
