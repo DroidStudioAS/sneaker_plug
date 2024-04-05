fc53119d9500c68656b140b239e005a0611c40c1
DroidStudioAS <as.droiddevteam@gmail.com>
2024-04-05 09:33:05 +0200

    made front end for sending order

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -21,10 +21,64 @@
         </div>
         <div class="payment_container">
             <p>Select A Payment Method</p>
-            <div class="icon_container">
-                <img src="{{asset("/res/icon_paypal.svg")}}" alt="">
-                <img src="{{asset("/res/icon_apple.svg")}}" alt="">
+            <div id="icon_container" class="icon_container">
+                <img onclick="handlePaymentMethodClick('paypal')" src="{{asset("/res/icon_paypal.svg")}}" alt="paypal">
+                <img onclick="handlePaymentMethodClick('apple_pay')" src="{{asset("/res/icon_apple.svg")}}" alt="apple_pay">
             </div>
+            <label for="contact_email">Email:</label>
+            <input id="contact_email" type="email" name="contact_email" class="input_text"
+                   @if(\Illuminate\Support\Facades\Auth::check())
+                   value="{{\Illuminate\Support\Facades\Auth::user()->email}}">
+                  @endif
+
+            <label for="contact_number">Phone Number:</label>
+            <input id="contact_number" type="text" name="contact_number" class="input_text">
+            <button onclick="sendOrder()" class="input_submit">Confirm</button>
         </div>
     </div>
+    <script>
+        let paymentMethod = "";
+        let contactNumber = $("#contact_number");
+        let contactEmail = $("#contact_email");
+
+        function handlePaymentMethodClick(selectedPaymentMethod){
+            paymentMethod=selectedPaymentMethod;
+            $("#icon_container").children().each(function (){
+                if($(this).attr("alt")===paymentMethod){
+                    $(this).css("opacity","1")
+                    return;
+                }
+                $(this).css("opacity","0.5")
+            })
+
+            console.log(paymentMethod);
+        }
+        function validateOrderParams(){
+            if(paymentMethod==="" || contactEmail.val()==="" || contactNumber.val()===""
+                || contactEmail.val()===null || contactNumber.val()===null){
+               return false;
+            }
+            return true;
+        }
+        function sendOrder(){
+            if(!validateOrderParams()){
+                alert("Please Fill Out All The Required Fields")
+                return;
+            }
+           $.ajax({
+               url:"",
+               type:"POST",
+               data:{
+                   "token": $('meta[name="csrf-token"]').attr('content'),
+                   "payment_method":paymentMethod,
+                   "contact_email":contactEmail.val(),
+                   "contact_number":contactNumber.val()
+               },
+               success:function (response){
+                   console.log(response);
+               }
+           })
+
+        }
+    </script>
 @endsection
