f8061dd41ccc2acb0e10bb461fabd023c9429432
DroidStudioAS <as.droiddevteam@gmail.com>
2024-04-15 08:08:08 +0200

    added toggle button in admin edit product to only display 1 form at a time

--- resources/views/admin/edit_product.blade.php
+++ resources/views/admin/edit_product.blade.php
@@ -1,34 +1,38 @@
 @extends("layouts.admin_layout")
 @section("admin_content")
-    <div class="form_container">
-        <form action="{{route("product.edit",["product"=>$product])}}" method="post"
-              enctype="multipart/form-data"
-              style="display: flex; flex-flow: column nowrap; align-items: center">
-            @if(session("message_product"))
-               <p class="success_response">{{session("message_product")}}</p>
-            @endif
-            <h2>Edit Product: Enter The Values You Want To Change</h2>
-            {{csrf_field()}}
-            <select id="new_brand" name="category_id" class="input_text">
-                @foreach($categories as $category)
-                    <option @if($category->id===$product->category_id)
-                                selected
-                            @endif
-                            value="{{$category->id}}">{{$category->name}}</option>
-                @endforeach
-            </select>
-            <input value="{{$product->Name}}" placeholder="Product Name" name="Name" id="new_name" type="text" class="input_text">
-            <input value="{{$product->price}}" placeholder="Product Price" name="price" id="new_price" type="number" class="input_text">
-            <input name="image_name" id="new_image" type="file" class="input_text">
-            <label for="description">Product Description</label>
-            <textarea name="description" id="new_desc"  class="input_message">
+        <h2>Editing: {{$product->category->name}} {{$product->Name}}</h2>
+        <div id="toggleContainer" class="toggleContainer">
+            <div onclick="displayClickedForm('editProductTrigger')" id="editProductTrigger" class="toggleButton">Edit Product Information</div>
+            <div onclick="displayClickedForm('editSizeTrigger')" id="editSizeTrigger" class="toggleButton">Edit Product Sizes</div>
+            <div onclick="displayClickedForm('addSizeTrigger')" id="addSizeTrigger" class="toggleButton">Add Product Size</div>
+        </div>
+
+        <div id="form_container">
+            <form id="editProductForm" class="edit_form" action="{{route("product.edit",["product"=>$product])}}" method="post"
+                  enctype="multipart/form-data">
+                @if(session("message_product"))
+                    <p class="success_response">{{session("message_product")}}</p>
+                @endif
+                <h2>Edit Product: Enter The Values You Want To Change</h2>
+                {{csrf_field()}}
+                <select id="new_brand" name="category_id" class="input_text">
+                    @foreach($categories as $category)
+                        <option @if($category->id===$product->category_id)
+                                    selected
+                                @endif
+                                value="{{$category->id}}">{{$category->name}}</option>
+                    @endforeach
+                </select>
+                <input value="{{$product->Name}}" placeholder="Product Name" name="Name" id="new_name" type="text" class="input_text">
+                <input value="{{$product->price}}" placeholder="Product Price" name="price" id="new_price" type="number" class="input_text">
+                <input name="image_name" id="new_image" type="file" class="input_text">
+                <label for="description">Product Description</label>
+                <textarea name="description" id="new_desc"  class="input_message">
                 {{$product->description}}
             </textarea>
-            <input id="new_submit" type="submit" class="input_submit">
-        </form>
-        <div class="size_forms">
-            <form id="editSizeForm"
-                style="display: flex; flex-flow: column nowrap; align-items: center">
+                <input id="new_submit" type="submit" class="input_submit">
+            </form>
+            <form id="editSizeForm" class="edit_form">
                 {{csrf_field()}}
                 <p class="success_response" id="editSizeSr"></p>
                 <h2>Edit Product Sizes And Stock</h2>
@@ -45,8 +49,7 @@
                 </div>
             </form>
 
-            <form action="{{route("product.add.size",["product"=>$product])}}" method="post"
-                style="display: flex; flex-flow: column nowrap; align-items: center">
+            <form id="addSizeForm" class="edit_form" action="{{route("product.add.size",["product"=>$product])}}" method="post">
                 {{csrf_field()}}
                 <h2>Add A Size</h2>
                 <label for="size">Size</label>
@@ -57,11 +60,31 @@
             </form>
         </div>
 
-    </div>
+
 
     <script>
         let sizeId = -1;
         let sizeInFocus =-1;
+
+        //forms
+
+        function displayClickedForm(key){
+            $("#toggleContainer").children().each(function (){
+                if($(this).attr("id")===key){
+                    $(this).css("opacity",1);
+                    return
+                }
+                $(this).css("opacity",0.5);
+            })
+            //display the form
+            $("#form_container").children().each(function (){
+                if($(this).attr("id")===key.replace("Trigger","Form")){
+                    $(this).css("display","flex");
+                    return;
+                }
+                $(this).css("display","none")
+            })
+        }
 
         function displayAmountContainer(size){
             sizeId=size.id;
